d9036011eb8f4ddade1c36e69ff46b535d96832e
Hin <tonghohin77@gmail.com>
2023-03-27 22:15:28 -0300

    Registration SMS

--- server/twilio/twilio.js
+++ server/twilio/twilio.js
@@ -4,13 +4,24 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
-function sendSMS(phoneNumber) {
+function sendNotificationSMS(phoneNumber) {
     return client.messages.create({
-        body: "TESTING MESSAGE",
+        body: `*** Notification Testing Message ***`,
         from: "+14752628564",
         to: `+1${phoneNumber}`
     });
 }
-sendSMS("7783213740");
 
-module.exports = sendSMS;
+function sendRegistrationSMS(phoneNumber, patientName, appointmentDate, appointmentTime) {
+    return client.messages.create({
+        body: `
+        Hi ${patientName},
+        Your appointment on ${appointmentDate} at ${appointmentTime} is confirmed.
+        `,
+        from: "+14752628564",
+        to: `+1${phoneNumber}`
+    });
+}
+sendRegistrationSMS("7783213740", "KK", "13-2-2022", "13:00");
+
+module.exports = { sendNotificationSMS, sendRegistrationSMS };
