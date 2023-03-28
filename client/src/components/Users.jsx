6ee8201f57a17def11ac92df82783eca7553e8da
Svitlana <Svetlanalynne@gmail.com>
2023-03-28 12:20:41 -0300

    favicon added

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react';
 
 
+        // DELETE PATIENTS - CLIENT SIDE
+
+// function addDelEventListeners()
+// {
+//     const deleteBtns = document.querySelectorAll('.buttonDel');
+    
+//     for (let button of deleteBtns)
+//         {   
+//             console.log(e.target);
+//             // button.addEventListener('click',(e)=>deletePatient(e.target));
+//         }
+    
+
+
+      
+// };
 
 function Users ({user,index}) {
 
     const [editBtn, setEditButton] = useState('Edit');
+
+
+    // function deletePatient(e)
+    //     {
+    //         // fetch('http://localhost:5555/artist/'+ e.value,{method:'delete'});
+    //         console.log(e.target);
+    //     } 
 
     return (
        
@@ -18,7 +41,7 @@ function Users ({user,index}) {
                 <p type="text" className="textTypeData">{user.symptoms}</p>
                 <button id="submit"  disabled>{editBtn}</button>  
                 <button id="buttonDone" >Done</button>  
-                <button id="buttonDel" >X</button>
+                {/* <button id="buttonDel" onClick={deletePatient}>X</button> */}
             </div>
 
             </div>
