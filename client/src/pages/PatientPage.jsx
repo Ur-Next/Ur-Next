231ac7bd052758e69551f0a6a2ba5888d32bb7fe
Svitlana <Svetlanalynne@gmail.com>
2023-04-05 21:47:27 -0300

    patient page form ui

--- client/src/pages/PatientPage.jsx
+++ client/src/pages/PatientPage.jsx
@@ -1,69 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+
 
 function InfoForPatient() {
+
   const [userTel, setUserTel] = useState("");
-
-  // const [usersData, setUsersData] = useState([]);
-  const [responseData, setResponseData] = useState({
-    appointmentTime: "",
-    peopleAhead: "",
-  });
+  const [responseData, setResponseData] = useState({appointmentTime: "", peopleAhead: ""});
   const [isFormShown, setIsFormShown] = useState(true);
-  const userId = "6426e467d26cef4f2ce4b07f";
 
-  // useEffect(() => {
 
-  //     fetch("/users")
-  //     .then((res) => res.json()
-  //     .then ((usersData) => {
-
-  //     const appointmentTime  = usersData.filter((x) => x._id === userId)[0].appointmentTime;
-
-  //     const timeRemaining = new Date()
-  //     console.log('Time is ', timeRemaining);
-
-  //         setAppointmentTime(appointmentTime)
-
-  //     }))},[]);
 
   function handleSubmitTel() {
     fetch(`/users/${userTel}`).then((res) =>
       res.json().then((usersData) => {
-        setResponseData({
-          appointmentTime: usersData.appointmentTime,
-          peopleAhead: usersData.peopleAhead,
-        });
+
+        setResponseData({ appointmentTime: usersData.appointmentTime, peopleAhead: usersData.peopleAhead });
+        setIsFormShown(false);
       })
     );
-  }
+  };
 
-  function handleChange(e) {
+  function handleFormChange(e) {
     setUserTel(e.target.value);
   }
 
   return (
     <>
-      <img
-        className="logoGrayedOut"
-        src="./img/Logo_urnext_400.png"
-        alt="Logo Urnext"
-      ></img>
-      {isFormShown ? (
-        <form id="patientTelInput">
-          <input type="tel" onChange={handleChange} value={userTel}></input>
-          <button type="submit" onClick={handleSubmitTel}>
-            submit
-          </button>
+      <img className="logoGrayedOut" src="./img/Logo_urnext_400.png" alt="Logo Urnext"></img>
+      
+      {isFormShown ?
+
+        <form class="patientMainContainer">
+          <p className="messageText">Enter your phone number<br />to see when you are going to be admitted</p>
+          <input type="tel" placeholder="xxxxxxxxxx" onChange={handleFormChange} value={userTel}></input>
+          <button type="submit" onClick={handleSubmitTel}>Submit</button>
         </form>
-      ) : (
-        <div id="patientMainContainer">
+      :
+        <div class="patientMainContainer">
           <p className="messageText">Your estimate appointment time</p>
           <div id="timeAppointment">{responseData.appointmentTime}</div>
           <p className="messageText">Patients in the line ahead of you</p>
           <div id="count">{responseData.peopleAhead}</div>
           <p className="messageText">Estimated time remaining {" __ "} min</p>
         </div>
-      )}
+      }
 
       {/* <div id="patientMainContainer">
                 <p className="messageText">Your estimate appointment time</p>
