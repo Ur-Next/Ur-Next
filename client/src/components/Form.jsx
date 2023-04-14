bc6a9e3635e160ee45292d8a002e76b9114ed35e
Svitlana <Svetlanalynne@gmail.com>
2023-04-13 22:59:42 -0300

    validation done

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -32,7 +32,9 @@ function Form({toggleRerender}) {
                 <input type="date" name="appointmentDate" required value={formData.appointmentDate} onChange={handleFormDataChange}></input>
                 <input type="time"  name="appointmentTime" required value={formData.appointmentTime} onChange={handleFormDataChange}></input>
                 <input className="textTypeData" type="tel" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" name="phone" placeholder="Tel." required value={formData.phone} onChange={handleFormDataChange}></input>
+                <div id="requirementsTel">Your telephone must be 10 characters, no other symbols.</div>
                 <input className="textTypeData" type="email" placeholder="Email" name="email" required value={formData.email} onChange={handleFormDataChange}></input>
+                <div id="requirementsEmail">Please enter a valid email address</div>
                 <input className="textTypeData" type="text" placeholder="Symptoms" name="symptom" value={formData.symptom} onChange={handleFormDataChange}></input>
                 <button onClick={handleFormSubmit}>Submit</button>
                 <div id="emptySpace"></div>
